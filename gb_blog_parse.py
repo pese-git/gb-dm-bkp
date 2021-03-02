d5d8ce10aebfced3b6fefb14a115fd4ab832f3cd
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2021-03-02 19:02:13 +0300

    implemented task

--- gb_blog_parse.py
+++ gb_blog_parse.py
@@ -3,13 +3,14 @@ import typing
 from urllib.parse import urljoin
 import bs4
 
-from database.db import Database
+import pymongo
+
 from datetime import datetime
 
 
 class GbBlogParse:
-    def __init__(self, start_url, database: Database):
-        self.db = database
+    def __init__(self, start_url, db_client):
+        self.db = db_client["gb_data_mining_20_04_2021"]
         self.start_url = start_url
         self.done_urls = set()
         self.tasks = []
@@ -35,7 +36,6 @@ class GbBlogParse:
         for link in self.__create_set_links(url, tag_list):
             if link not in self.done_urls:
                 task = self._get_task(link, callback)
-                self.done_urls.add(link)
                 self.tasks.append(task)
 
     def _parse_date(self, date_str):
@@ -52,11 +52,13 @@ class GbBlogParse:
         author_name_tag = soup.find("div", attrs={"itemprop": "author"})
         post_title = soup.find("h1", attrs={"class": "blogpost-title"}).text
         post_date = soup.find("time", attrs={"itemprop": "datePublished"}).attrs.get("datetime")
+        post_id = soup.find("comments").attrs.get("commentable-id")
         data = {
             "post_data": {
                 "url": url,
                 "title": post_title,
                 "create_at": self._parse_date(post_date),
+                "id": post_id,
             },
             "author": {
                 "name": author_name_tag.text,
@@ -66,7 +68,17 @@ class GbBlogParse:
                 {"name": tag.text, "url": urljoin(url, tag.attrs.get("href"))}
                 for tag in soup.find_all("a", attrs={"class": "small"})
             ],
+            "comments_data": self._get_comments(post_id),
         }
+        return data
+
+    def _get_comments(self, post_id) -> list:
+        # https://geekbrains.ru/api/v2/comments?commentable_type=Post&commentable_id=2541&order=desc
+        # https://geekbrains.ru/api/v2/comments?commentable_type=Post&commentable_id=2543&order=desc
+        # https://geekbrains.ru/api/v2/comments?commentable_table=Post&commentable_id=$2541&order=desc
+        api_path = f"/api/v2/comments?commentable_type=Post&commentable_id={post_id}&order=desc"
+        response = self._get_response(self.start_url + api_path)
+        data = response.json()
         return data
 
     def _get_task(self, url, callback: typing.Callable) -> typing.Callable:
@@ -74,22 +86,29 @@ class GbBlogParse:
             soup = self._get_soup(url)
             return callback(url, soup)
 
+        if url in self.done_urls:
+            return lambda *_, **__: None
+        self.done_urls.add(url)
         return task
 
     def run(self):
-        self.tasks.append(self._get_task(self.start_url, self._parse_feed))
-        self.done_urls.add(self.start_url)
+        self.tasks.append(self._get_task(self.start_url + "/posts/", self._parse_feed))
+        self.done_urls.add(self.start_url + "/posts/")
         for task in self.tasks:
             result = task()
             if isinstance(result, dict):
                 print(result)
-                self.db.create_post(result)
+                self.save(result)
             print(1)
+
+    def save(self, data):
+        collection = self.db["post"]
+        collection.insert_one(data)
 
 
 if __name__ == "__main__":
-    db = Database("sqlite:///gd_blog.db")
-    url = "https://geekbrains.ru/posts/"
-    parser = GbBlogParse(url, db)
+    db_client = pymongo.MongoClient("mongodb://localhost:27017")
+    url = "https://geekbrains.ru"
+    parser = GbBlogParse(url, db_client)
 
     parser.run()
